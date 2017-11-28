4fde4f4e7b4ebe2f490bbbe344f48f0ede5aeeb9
Cole Tanigawa-Lau <coletl@nyu.edu>
2017-11-27 16:10:16 -1000

    Add state abbreviations to 2010 apportionment data set.

--- data-raw/scrape_congrapp10.R
+++ data-raw/scrape_congrapp10.R
@@ -2,8 +2,11 @@ library(pdftools)
 library(readr)
 library(data.table)
 
+# Read in data from Census Bureau site
 txt <- pdf_text("https://www.census.gov/content/dam/Census/library/publications/2011/dec/c2010br-08.pdf")
 tbl <- read_lines(txt[grep("^Table 1", txt)])
+
+# Clean
 header <- tbl[1:8]
 tbl2 <- tbl[13:62]
 tbl3 <- gsub("[ .]{2,}", ";", tbl2)
@@ -23,5 +26,14 @@ tbl7 <- melt(tbl6, id.vars = "state",
              variable.name = "census_year")
 tbl7[ , census_year := gsub("[^0-9]", "", census_year)]
 
-seats10 <- tbl7
-use_data(seats10)
+# Add state abbreviations
+data(state)
+stateDT <- data.table(state = state.name,
+                      state_abb = state.abb)
+tbl7[stateDT,
+     state_abb := i.state_abb,
+     on = "state"
+     ]
+
+seats10 <- tbl7[ , .(state, state_abb, census_year, congr_seats)]
+devtools::use_data(seats10, overwrite = TRUE)
