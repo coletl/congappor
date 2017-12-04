285ca5395e11d350c4187b8df612eff5a47937c6
Cole Tanigawa-Lau <coletl@nyu.edu>
2017-12-04 00:10:59 -1000

    Scrape data on statehood dates.

---
+++ data-raw/scrape_statehood.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(data.table)
+library(stringr)
+
+statehood <- readLines("https://www.50states.com/statehood.htm")
+
+state <- str_extract(statehood,
+                     "(?<=<td><b>)[A-Z a-z]+(?=</b></td>)") %>%
+  .[!is.na(.)]
+date <- str_extract(statehood,
+                    "(?<=<td>)[[:alnum:] .,]+(?=<br>[[:alnum:]<> ]+</td>)")  %>%
+  .[!is.na(.)] %>% str_trim() %>% gsub(".", "", x = ., fixed = TRUE) %>%
+  gsub("Sept", "Sep", x = ., fixed = TRUE)
+
+statehood <- data.table(state,
+                        date = as.Date(date, "%b %d, %Y"))
+
+devtools::use_data(statehood)
