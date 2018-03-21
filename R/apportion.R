bb00c2ad1d5b6a3f116206e6bff3e8a255777dca
Cole Tanigawa-Lau <coletl@nyu.edu>
2018-03-21 11:22:14 +0100

    Add option for voting seats from Guam. Add proper documentation of apportion().

--- R/apportion.R
+++ R/apportion.R
@@ -1,25 +1,55 @@
-#' @import data.table
+#' Calculate U.S. congressional apportionment.
+#'
+#' \code{apportion()} simulates U.S. congressional apportionment with a state-level
+#' population data set you provide.
+#'
+#' Currently, \code{apportion()} can implement only the
+#' \link{https://www.census.gov/population/apportionment/about/computing.html}{method of equal proportions}.
+#' This method has been used since 1941. Support for obsolete methods may be added later.
 
+#' @param   pop_data           A data frame of population values for each state. State names will be internally coerced to uppercase.
+#' @param   total_seats        The total number of voting seats in the hypothetical congress. This number was fixed at the default (435) in 1911.
+#' @param   apportion_year     A character string (or coercible) indicating the year of apportionment to simulate.
+#'                               For now, this parameter affects only the number of states in the union, not the apportionment method,
+#'                               so you're better off just supplying \code{pop_data} with population values for only the states in which you're interested.
+#' @param   DC_seats           A logical indicating whether to allocate voting seats to Washington, D.C.
+#' @param   PR_seats           A logical indicating whether to allocate voting seats to Puerto Rico.
+#' @param   GU_seats           A logical indicating whether to allocate voting seats to Guam.
+#' @param   min_seats          The minimum (starting) number of seats per state. The default is 1.
+#' @param   store_priority     A logical indicating whether to store the priority scores calculated for each state-seat combination.
+#'                               Scores are stored as a wide \code{data.table} object named \code{priority_scores} in the enclosing environment,
+#' @param   store_seat_order   A logical indicating whether to store state names in the order that seats were allocated.
+#'                               The seat order is stored as a character vector named \code{seat_order} in the enclosing environment.
+#' @return A two-column \code{data.table} containing the name of each state and the number of seats allocated to it.
+#'
+#' @import data.table methods
+#'
 apportion <-
   function(pop_data,
-           total_seats = 435, # Total number of voting seats in the hypothetical congress. This number was fixed at the default (435) in 1911.
-           apportion_year = NULL, # Year of apportionment to simulate, affects only number of states in the union. Overrides `states` parameter.
-           # Currently, `apportion()` supports only the method of equal proportions, used only since 1940.
-           states = 50, # Number of states.
-           DC_seats = FALSE, # Whether DC should be eligible for voting seats in congress.
-           PR_seats = FALSE, # Whether Puerto Rico should be eligible for voting seats in congress.
-           min_seats = 1, # Minimum (starting) number of seats per state.,
-           store_priority = FALSE, # Store, as a data.table named "priority_scores" in the enclosing environment, priority scores calculated for each seat.
-           store_seat_order = FALSE # Store, as a character vector named "seat_order" in the enclosing environment, state names in the order that seats were allocated.
+           total_seats = 435,
+           apportion_year = NULL,
+           DC_seats = FALSE,
+           PR_seats = FALSE,
+           GU_seats = FALSE,
+           min_seats = 1,
+           store_priority = FALSE,
+           store_seat_order = FALSE
            ){
-    # Equation from: https://www.census.gov/population/apportionment/about/computing.html
 
+    if(!require(data.table)) stop("apportion requires the data.table package.")
     if(!is.data.table(pop_data)) setDT(pop_data)
 
-    if(! DC_seats) pop_data <- pop_data[state != "District of Columbia"]
-    if(! PR_seats) pop_data <- pop_data[state != "Puerto Rico"]
+    pop_data[ ,
+              state := toupper(state)
+              ][ ,
+                 state := gsub("[[:punct:]]", " ", state, perl = TRUE)
+                 ]
 
-    if(!is.null(apportion_year) || states != 50) {
+    if(! DC_seats) pop_data <- pop_data[state != "DISTRICT OF COLUMBIA"]
+    if(! PR_seats) pop_data <- pop_data[state != "PUERTO RICO"]
+    if(! GU_seats) pop_data <- pop_data[state != "GUAM"]
+
+    if(!is.null(apportion_year)) {
       data("statehood")
       apportion_jan <- as.Date(paste(apportion_year, "01", "01", sep = "-"))
 
@@ -28,7 +58,7 @@ apportion <-
       valid_states <- pop_data
       }
 
-    rem_seats <- total_seats - min_seats * (nrow(valid_states) + DC_seats + PR_seats)
+    rem_seats <- total_seats - min_seats * (nrow(valid_states) + DC_seats + PR_seats + GU_seats)
 
     # Compute multipliers
     # n = number of seats, if the state were to gain a seat (seats + 1)
